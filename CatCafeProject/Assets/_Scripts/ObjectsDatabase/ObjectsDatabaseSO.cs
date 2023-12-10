ac16d9dd172be505a2e2f9fc5c82fd2aaa469072
alvaro51star <alvaro51star@hotmail.es>
2023-12-11 00:26:52 +0100

    Placement system update

---
+++ CatCafeProject/Assets/_Scripts/ObjectsDatabase/ObjectsDatabaseSO.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ObjectsDatabaseSO", menuName = "CatCafeProject/ObjectsDatabaseSO", order = 0)]
+public class ObjectsDatabaseSO : ScriptableObject
+{
+    public List<ObjectData> objectDatas;
+}
+
+[Serializable]
+public class ObjectData
+{
+    [field: SerializeField]
+    public string Name { get; private set; }
+
+    [field: SerializeField]
+    public int ID { get; private set; }
+    
+    [field: SerializeField]
+    public Vector2Int Size { get; private set; } = Vector2Int.one;
+    
+    [field: SerializeField]
+    public GameObject Prefab { get; private set; }
+
+    [field: SerializeField]
+    public int Prize {get; private set;} = 0;
+}
