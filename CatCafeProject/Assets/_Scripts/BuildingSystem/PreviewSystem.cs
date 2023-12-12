d38af38a9e352aa6b57d013aba905c00ef6f8ae8
alvaro51star <alvaro51star@hotmail.es>
2023-12-12 23:24:32 +0100

    Tutorial parte 4 a medio hacer

---
+++ CatCafeProject/Assets/_Scripts/BuildingSystem/PreviewSystem.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewSystem : MonoBehaviour
+{
+    [SerializeField]
+    private float previewYOffset = 0.06f;
+
+    [SerializeField]
+    private GameObject cellIndicator;
+    private GameObject previewObject;
+
+    [SerializeField]
+    private Material previewMaterialsPrefab;
+    private Material previewMaterialsInstance;
+
+    private Renderer cellIndicatorRenderer;
+    
+
+    private void Start()
+    {
+        previewMaterialsInstance = new Material(previewMaterialsPrefab);
+        cellIndicator.SetActive(false);
+        cellIndicatorRenderer = cellIndicator.GetComponentInChildren<Renderer>();
+    }
+
+    public void StartShowingPlacementPreview(GameObject prefab, Vector2Int size)
+    {
+        previewObject = Instantiate(prefab);
+        PreparePreview(previewObject);
+        PrepareCursor(size);
+        cellIndicator.SetActive(true);
+    }
+
+    private void PreparePreview(GameObject previewObject)
+    {
+        Renderer[] renderers = previewObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            Material[] materials = renderer.materials;
+            for (int i = 0; i < materials.Length; i++)
+            {
+                materials[i] = previewMaterialsInstance;
+            }
+            renderer.materials = materials;
+        }
+    }
+
+    private void PrepareCursor(Vector2Int size)
+    {
+        if (size.x > 0 || size.y > 0)
+        {
+            cellIndicator.transform.localScale = new Vector3(size.x, 1, size.y);
+            cellIndicatorRenderer.material.mainTextureScale = size;
+        }
+    }
+
+    public void StopShowingPreview()
+    {
+        cellIndicator.SetActive(false);
+        Destroy(previewObject);
+    }
+
+    public void UpdatePosition(Vector3 position, bool validity)
+    {
+        MovePreview(position);
+        MoveCursor(position);
+        ApplyFeedBack(validity);
+    }
+
+    private void ApplyFeedBack(bool validity)
+    {
+        Color c = validity ?  Color.white: Color.red;
+        cellIndicatorRenderer.material.color = c;
+        c.a = 0.5f;
+        previewMaterialsInstance.color = c;
+    }
+
+    private void MoveCursor(Vector3 position)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void MovePreview(Vector3 position)
+    {
+        previewObject.transform.position = new Vector3(position.x, position.y + previewYOffset, position.z);
+    }
+}
