c5f2b31d87ea727d5ab69f5d0cf64a3b9f366793
elenarojore <elena.red.kings@gmail.com>
2023-12-16 20:16:55 +0100

    ModePlayer

---
+++ CatCafeProject/Assets/_Scripts/CafeteriaMode/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool canMove = true; //para facilitar pausa
+    [SerializeField, Range(2, 10)] private float speed;
+    private Vector3 directionInput;
+    private float directionX, directionZ;
+
+    void Update()
+    {
+        GetInputDirection();
+
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (canMove)
+        {
+            transform.Translate(directionInput.normalized * (speed * Time.deltaTime));
+        }
+        //else anim de idle
+    }
+
+    private Vector3 GetInputDirection()//cambiar esto segun eventos?
+    {
+        directionX = Input.GetAxisRaw("Horizontal");
+        directionZ = Input.GetAxisRaw("Vertical");       
+        directionInput = new Vector3(directionX, 0, directionZ);
+
+        return directionInput;
+    }    
+}
