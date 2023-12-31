a792036b7b5a641ff216b8674d0ce675bb367851
elenarojore <elena.red.kings@gmail.com>
2023-12-31 14:51:13 +0100

    script interactiveObject como proyecto anterior

--- CatCafeProject/Assets/_Scripts/CafeteriaMode/InteractiveObject.cs
+++ CatCafeProject/Assets/_Scripts/CafeteriaMode/InteractiveObject.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class InteractiveObject : MonoBehaviour
 {
-    private void OnTriggerStay(Collider other)
+    public delegate void PlayerInside();
+    public static event PlayerInside OnPlayerInside;
+
+    public delegate void PlayerOutside();
+    public static event PlayerOutside OnPlayerOutSide;
+
+    protected virtual void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            if (OnPlayerInside != null)
+            {
+                OnPlayerInside();
+            }
+        }
+    }
+
+    protected virtual void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (OnPlayerOutSide != null)
+            {
+                OnPlayerOutSide();
+            }
+        }
     }
 }
