952680bef9692df82546660bb93c66cb0c41f84c
EHaoqin <espee.delgado@gmail.com>
2024-01-25 04:56:47 +0100

    animaciones gatos sin comprobar y loading panel

--- CatCafeProject/Assets/_Scripts/UI/Canvas/UIMenus.cs
+++ CatCafeProject/Assets/_Scripts/UI/Canvas/UIMenus.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIMenus : MonoBehaviour
 {
     private GameObject m_player;
     private GameObject m_uiInput;
     private UIManager m_UIManager;
+    public Slider m_slider;
 
     private int m_initialScene = 0;
     private int m_gameScene = 1;
@@ -104,5 +106,25 @@ public class UIMenus : MonoBehaviour
         GameManager.instance.ChangeGameMode(GameModes.Decoration);
         m_UIManager.DesactivateAllUIGameObjects();
         //meter pantalla de carga
+    }
+
+    public void LoadingPanel(int gameSceneNum)
+    {
+        StartCoroutine(LoadAsync(gameSceneNum));
+    }
+
+    IEnumerator LoadAsync(int gameSceneNum)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(gameSceneNum);
+
+        m_UIManager.ActivateUIGameObjects(m_UIManager.loadingPanel, true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            Debug.Log(progress);
+            m_slider.value = progress;
+            yield return null;
+        }
     }
 }
